9bf9771390ea6485c6dfc42e622c18f1c4f0ac46
sergeym <sergey.malakhov@ls2.superfund.local>
2025-01-24 14:43:03 +0100

    Fixed bug with not proper call of useDoc()

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/MDXComponentContainer.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/MDXComponentContainer.tsx
@@ -1,15 +1,13 @@
-import { useDoc } from "@docusaurus/plugin-content-docs/client";
 import React from "react";
 
 // @ts-ignore
 export default function MDXComponentContainer({
   MDXComponent,
+  hideRightPanel,
 }: {
   MDXComponent: any;
+  hideRightPanel?: boolean;
 }): JSX.Element {
-  const metadata = useDoc();
-  const hideRightPanel = metadata.frontMatter.hide_right_panel;
-
   return (
     <div
       className={`col col--${hideRightPanel ? 12 : 7} openapi-left-panel__container`}
